35f3e6e04e01accdbbe935c9182c51b3608efeba
Nuwan Senevirathne <smpg.nuwan@gmail.com>
2023-09-14 22:27:52 +0530

    code optimization

--- script.js
+++ script.js
@@ -3,16 +3,19 @@ const cell = document.createElement('div');
 const cell_x = document.createElement('div');
 const cell_o = document.createElement('div');
 
+document.body.appendChild(board);
+
 board.id = 'board';
 board.className = 'board circle';
 
 
 cell.className = 'cell';
+const startGame = () => {
 
 // cell_x.className = 'cell x';
 // cell_o.className = 'cell circle';
 
-document.body.appendChild(board);
+
 
 
 
@@ -22,7 +25,9 @@ for(let i =0; i<9; i++) {
     newCell.id = `${i}`;
     board.appendChild(newCell);
 }
+}
 
+startGame();
 
 const allCells = document.querySelectorAll('.cell')
 
@@ -30,47 +35,54 @@ let isMarkO=true;
 
 const winningPatterns = [
     // Rows
-    [[0, 0], [0, 1], [0, 2]],
-    [[1, 0], [1, 1], [1, 2]],
-    [[2, 0], [2, 1], [2, 2]],
+    [0, 1, 2], 
+    [3, 4, 5], 
+    [6, 7, 8],
     // Columns
-    [[0, 0], [1, 0], [2, 0]],
-    [[0, 1], [1, 1], [2, 1]],
-    [[0, 2], [1, 2], [2, 2]],
+    [0, 3, 6], 
+    [1, 4, 7], 
+    [2, 5, 8],
     // Diagonals
-    [[0, 0], [1, 1], [2, 2]],
-    [[0, 2], [1, 1], [2, 0]],
-  ];
+    [0, 4, 8], 
+    [2, 4, 6]             
+];
 
-const finalBoard = [[],[],[]];
+const finalBoard = Array(9).fill(null);
+let isXNext = true;
+let winner = null;
+
+const checkWinner = () => {
+    for( const pattern of winningPatterns) {
+        [x,y,z] = pattern;
+
+        if (finalBoard[x] && finalBoard[x] === finalBoard[y] && finalBoard[x] === finalBoard[z]) {
+        
+            return winner=finalBoard[x];
+        } 
+
+        if (finalBoard.every(cell => cell)) {
+            return winner = 'Draw';
+        }
+    }
+}
 
 const handleCellClick = (e) => {
-    const idValue= parseInt(e.target.attributes.id.value);
-    
-    
-    if (isMarkO) {
-        // e.target.attributes.id.value += '+o'
-        if(finalBoard[0].length === 3 && finalBoard[1].length === 3 && finalBoard[2].length === 3) console.log('hello')
-        e.target.classList.value = 'cell circle';
-        isMarkO = false
-        board.className = 'board x';   
+    const cell = e.target;
+    const cellIndex = cell.id;
 
-        if (idValue < 3) finalBoard[0][idValue] = '0';
-        if (idValue >= 3 && idValue < 6) finalBoard[1][idValue - 3] = '0';
-        if (idValue >= 6 && idValue < 9) finalBoard[2][idValue - 6] = '0';
-        console.log(finalBoard[0], finalBoard[1], finalBoard[2])
-        return;
+    if(!winner) {
+        finalBoard[cellIndex] = !isXNext ? 'X' : 'O'
+        board.className = isXNext ? 'board x' : 'board circle'
+        cell.classList.add(!isXNext ? 'x' : 'circle')
+        isXNext = !isXNext;
+
+        const wonby = checkWinner();
+
+        if (winner) {
+            winner = 'Draw' ? window.alert(`It's a draw`) :window.alert(`${wonby} won`);
+            startGame();
+        }
     }
-    // e.target.attributes.id.value += '+x' 
-    e.target.classList.value = 'cell x'
-    isMarkO = true
-    board.className = 'board circle';
-    if (idValue < 3) finalBoard[0][idValue] = 'X';
-    if (idValue >= 3 && idValue < 6) finalBoard[1][idValue - 3] = 'X';
-    if (idValue >= 6 && idValue < 9) finalBoard[2][idValue - 6] = 'X';
-
-    console.log(finalBoard[0], finalBoard[1], finalBoard[2])
-    if(finalBoard[0].length === 3 && finalBoard[1].length === 3 && finalBoard[2].length === 3) console.log('hello')
 }
 
 
